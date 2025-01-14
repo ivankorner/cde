54bcded5591f776dde850ba83f23af6080951e89
ivankorner <ivankorner@gmail.com>
2025-01-14 09:18:10 -0300

    Subiendo Mejores

--- vistas/login.php
+++ vistas/login.php
@@ -1,65 +1,63 @@
-<section class="h-100 gradient-form" style="background-color: #eee;">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-xl-10">
-                <div class="card rounded-3 text-black">
-                    <div class="row g-0">
-                        <div class="col-lg-6">
-                            <div class="card-body p-md-5 mx-md-4">
-
-                                <div class="text-center">
-                                    <img src="./img/logo.png"
-                                        style="width: 185px;" alt="logo">
-                                    <h4 class="mt-1 mb-5 pb-1"></h4>
-                                    <br>
+<section class="h-100">
+    <div class="container h-100">
+        <div class="row justify-content-sm-center h-100">
+            <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+                <div class="text-center my-5">
+                    <img src="./img/logo.png" alt="logo" width="185">
+                </div>
+                <div class="card shadow-lg">
+                    <div class="card-body p-5">
+                        <h1 class="fs-4 card-title fw-bold mb-4">Login</h1>
+                        <form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+                            <div class="mb-3">
+                                <label class="mb-2 text-muted">Usuario</label>
+                                <input type="text" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" require class="form-control" required autofocus />
+                                <div class="invalid-feedback">
+                                    Usuario es requerido
                                 </div>
-
-                                <form action="" method="POST" autocomplete="off">
-                                    <p>Ingrese sus credenciales de acceso</p>
-
-                                    <div data-mdb-input-init class="form-outline mb-4 input-group">
-                                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill-check"></i></span>
-
-                                        <input type="text" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" require class="form-control"
-                                            placeholder="Usuario" />
-
-
-
-                                    </div>
-
-                                    <div data-mdb-input-init class="form-outline mb-4 input-group">
-                                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-key-fill"></i></i></span>
-                                        <input type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" class="form-control" placeholder="Password" />
-
-                                    </div>
-
-                                    <div class="text-center pt-1 mb-5 pb-1">
-
-                                        <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Ingresar</button>
-
-                                        <a class="text-muted" href="#!">Olvidó su Password?</a>
-                                    </div>
-
-                                    <div class="d-flex align-items-center justify-content-center pb-4">
-                                        <p class="mb-0 me-2">No tiene cuenta?</p>
-                                        <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-danger">Crear</button>
-                                    </div>
-
-                                    <?php
-                                    if (isset($_POST['login_usuario']) && isset($_POST['login_clave'])) {
-                                        require_once "./php/main.php";
-                                        require_once "./php/iniciar_sesion.php";
-                                    }
-                                    ?>
-
-                                </form>
-
                             </div>
-                        </div>
-                       
+
+                            <div class="mb-3">
+                                <div class="mb-2 w-100">
+                                    <label class="text-muted" for="password">Password</label>
+                                    <a href="./olvido_password.php" class="float-end">
+                                        Olvidó su Password?
+                                    </a>
+                                </div>
+                                <input type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" class="form-control" required />
+                                <div class="invalid-feedback">
+                                    Password es requerido
+                                </div>
+                            </div>
+
+                            <div class="d-flex align-items-center">
+                                <div class="form-check">
+                                    <input type="checkbox" name="remember" id="remember" class="form-check-input">
+                                    <label for="remember" class="form-check-label">Recordarme</label>
+                                </div>
+                                <button type="submit" class="btn btn-primary ms-auto">
+                                    Login
+                                </button>
+                            </div>
+                            <?php
+                            if (isset($_POST['login_usuario']) && isset($_POST['login_clave'])) {
+                                require_once "./php/main.php";
+                                require_once "./php/iniciar_sesion.php";
+                            }
+                            ?>
+                        </form>
                     </div>
+                    <div class="card-footer py-3 border-0">
+                        <div class="text-center">
+                            <a href="register.html" class="text-dark"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mt-5 text-muted">
+                    Copyright &copy; 2025 &mdash; Concejo Deliberante Eldorado
                 </div>
             </div>
         </div>
     </div>
 </section>
+
